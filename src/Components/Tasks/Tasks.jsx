913f4f0ed413b0bfa6dd6c408544a9682ba77fd5
eecohm <eecohmplustwo@gmail.com>
2025-04-18 15:19:37 +0545

    remove every thing

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -1,10 +1,8 @@
 // src/Components/Tasks/TaskAssignment.jsx
 function TaskAssignment() {
     return (
-      <div className="task-page">
-        <h2>Task Assignment</h2>
-        <p>Here you can assign and manage tasks.</p>
-      </div>
+     <>
+     </>
     );
   }
   
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,10 +1,8 @@
 // src/Components/Users/Users.jsx
 function Users() {
     return (
-      <div className="users-page">
-        <h2>User Management</h2>
-        <p>Here you can view and manage users.</p>
-      </div>
+      <>
+      </>
     );
   }
   
